f7e0f0c60864d41ef98f66fc11682f180ec90585
Ismael Reyes <reyes19isma@gmail.com>
2020-12-18 01:29:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+console.log('Bienvenido! programa de tabla de multiplicar');
+
+console.log('');
+let numero= parseInt(prompt('Ingresa el numero'));
+
+console.log('');
+for(i=1; i<=10; i++){
+    console.log(numero + 'x'+ i+ '='+ numero*i)
+}
+
